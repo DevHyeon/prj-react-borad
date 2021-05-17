7db1eabe900d02c143b72abc1678aa37101dfff1
DESKTOP-7K61OQD\USER <yshss1@naver.com>
2021-05-17 15:47:47 +0900

    react-borad prj

---
+++ front-end/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import AppRouter from './component/route/RouterComponent'
+import NavBar from './component/route/NavBar'
+
+import Container from '@material-ui/core/Container'
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Container>
+          <AppRouter />
+        </Container>
+      </div>
+    )
+  }
+}
+
+export default App
